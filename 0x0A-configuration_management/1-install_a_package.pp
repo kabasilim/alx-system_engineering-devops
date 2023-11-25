ad656e8269bef6d25b3e8e1a69966b345d041e39
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-11-25 07:27:53 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,7 @@
 # Install a particular version of flask 2.1.0
 # using puppet
 package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3',
 }
