2197d5f41a58ed96d5a4cd0dfb552e0a791e0616
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-11-26 18:20:07 -0800

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+# using Puppet to make changes to the configuration file.
+# so that one can connect to a server without typing a password.
+
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
