6874a586ba581acf2d7148e5c77d038745ce1f2f
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-12-10 00:57:45 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
 # installs flask version 2.1.0 with pip3
-
-class flask_install {
-  package { 'python3-pip':
-    ensure => present,
-  }
-
-  exec { 'install_flask':
-    command => 'pip3 install flask==2.1.0',
-    user    => 'root',
-    path    => '/usr/local/bin:/usr/bin:/bin',
-    require => Package['python3-pip'],
-  }
+package { 'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3',
 }
-
