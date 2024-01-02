2c35bb5eef351bc0912aabc60112b02f966500d1
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-01-02 12:57:42 -0800

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-"""comment"""
+"""This script makes a request to a REST API"""
 
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com"
-    user_id = int(sys.argv[1])
-    user_endpoint = "{}/user/{}".format(url, user_id)
-    name = requests.get(user_endpoint).json().get("name")
-    tasks_endpoint = "{}/todos".format(url)
-    tasks = requests.get(tasks_endpoint).json()
-    user_tasks = [task for task in tasks if tasks.get("userId") == user_id]
-    completed_tasks = [task for task in tasks if tasks.get("completed")]
-    print("Employee {} is done with tasks({}/{})"
-          .format(name, len(completed_tasks), len(user_tasks)))
 
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+if __name__ == "__main__":
+    id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_data = requests.get("{}/users/{}".format(base_url, id)).json()
+    todo_data = requests.get("{}/users/{}/todos".format(base_url, id)).json()
+
+    name = user_data.get('name')
+    task_length = len(todo_data)
+    completed_tasks = []
+
+    for value in todo_data:
+        if value.get('completed'):
+            completed_tasks.append(value.get('title'))
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, len(completed_tasks), len(todo_data)))
+
+    for value in completed_tasks:
+        print("\t {}".format(value))
