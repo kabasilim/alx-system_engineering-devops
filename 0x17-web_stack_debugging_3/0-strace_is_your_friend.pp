ed10580f537f315aaa47729c7a2da85332e3b50c
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-01-17 19:07:42 -0800

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+
+exec { 'fix-wordpress':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
